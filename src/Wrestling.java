d67cdedc5d81c6ae81af9a46d0d041c3930395c9
snusun <0515hwang@snu.ac.kr>
2022-03-21 22:33:43 +0900

    add files

---
+++ src/Wrestling.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Player implements Comparable<Player>{
+    int height;
+    int weight;
+    Player(int height, int weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+    @Override
+    public int compareTo(Player p){
+        return p.height-this.height;
+    }
+}
+
+public class Wrestling {
+    static int solution(ArrayList<Player> players){
+        int cnt = 0;
+        Collections.sort(players);
+        int max = Integer.MIN_VALUE;
+
+        for(Player p : players){
+            if(p.weight>max){
+                max = p.weight;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<Player> players = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            int h = in.nextInt();
+            int w = in.nextInt();
+            players.add(new Player(h, w));
+        }
+
+        System.out.println(solution(players));
+
+        return ;
+    }
+}
